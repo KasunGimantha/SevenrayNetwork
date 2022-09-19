8bedcc296ee9703c9db8757ed2f8a71aa4a5e6c4
Kasun Gimantha <ksngimantha@gmail.com>
2022-09-20 01:04:34 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+
+function popAlert1(){
+    var msgOne= "Mobile: +94 779 919 341";
+    alert(msgOne);
+}
+
+function popAlert2(){
+    var msgTwo= "Admission:5000LKR     Monthly fee:2000LKR";
+    alert(msgTwo);
+}
+
